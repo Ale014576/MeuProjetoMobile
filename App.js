cf08bf4116d022362310a86782d5ba2e0e4d7817
Ale <Joaoalejandrobrenisborges@gmail.com>
2025-03-26 14:38:04 -0300

    .

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
 import 'react-native-gesture-handler';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
-import Ho from './screens/Home';
-import Log from './screens/Login';
+import {Ho} from './screens/Home';
+import {Log} from './screens/Login';
 
 export default function App() {
+
   const Stack = createStackNavigator();
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name='login' component={Log}/>
-        <Stack.Screen name='home' component={Ho}/>
-
+        <Stack.Screen name='Login' component={Log}/>
+        <Stack.Screen name='Home' component={Ho}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
