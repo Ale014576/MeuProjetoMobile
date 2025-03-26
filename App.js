f480e3a75b1f8f611a7c308fe672587af9d0da9e
Ale <Joaoalejandrobrenisborges@gmail.com>
2025-03-26 14:29:49 -0300

    .

--- App.js
+++ App.js
@@ -1,22 +1,18 @@
-import { View, StyleSheet} from 'react-native';
-// Only import react-native-gesture-handler on native platforms
 import 'react-native-gesture-handler';
-import {Log} from './screens/Login';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
 import Ho from './screens/Home';
+import Log from './screens/Login';
 
 export default function App() {
+  const Stack = createStackNavigator();
   return (
-    <View style={styles.container}>
-        <Log/>
-        <Ho/>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='login' component={Log}/>
+        <Stack.Screen name='home' component={Ho}/>
 
-    </View>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1 
-  }
-})
